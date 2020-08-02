bb52e0e79a90d1b84a71c16edb5407ae70c664ef
innfi <innfi.world@gmail.com>
2020-08-02 23:02:57 +0900

    refactoring: implement with module structure

--- terraform/gateway.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "DemoGateway" {
-    vpc_id = aws_vpc.DemoVPC.id
-    tags = {
-        Name="DemoGateway"
-    }
-}
--- terraform/vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "DemoVPC" {
-    cidr_block = "10.0.0.0/16"
-    instance_tenancy = "default" 
-    enable_dns_hostnames = true
-    tags = {
-        Name = "Demo VPC"
-    }
-}
